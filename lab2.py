3c0c7288618cf65ae9c4c7e6ccca99a86fd6f692
Jaeson <jaesonmathew.22@ichat.sp.edu.sg>
2024-10-21 16:31:27 +0800

    Added median function

--- lab2.py
+++ lab2.py
@@ -10,10 +10,25 @@ def display_main_menu():
 def calc_average_temperature(values):
     return sum(values)/len(values)
 
+def calc_median_value(median):
+    sorted_median= sorted(median)
+    n = len(sorted_median)
+
+    if n%1==1:
+        median = sorted_median=[n//2]
+    else:
+        median = (sorted_median[n//2-1] + sorted_median[n//2])/2
+
+    return median        
+
+
 def main():
     output = display_main_menu()
     averageval= calc_average_temperature(output)
+    medianval=calc_median_value(output)
+
     print("Average value =",averageval)
+    print("Median value =",medianval)
 
 
 main()
