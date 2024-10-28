3d2a2cc37c9effd0cce4a3f96fff42b8a76f8612
Jaeson <jaesonmathew.22@ichat.sp.edu.sg>
2024-10-28 15:21:16 +0800

    Modified calculate_bmi function to return classification values

--- bmi.py
+++ bmi.py
@@ -1,14 +1,16 @@
 def calculate_bmi(height, weight):
-    print("Height =" + str(height))
-    print("Weight =" + str(weight))
-    bmi = weight/height**2
-    print("Your BMI is =" + str(bmi))
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+    bmi = weight / height ** 2
+    print("Your BMI is = " + str(bmi))
 
-    if bmi<18.5:
-        print ("Under Weight")
-    elif bmi>=18.5 and bmi<=25:
-        print ("Normal Weight")
-    elif bmi>25:
-        print("Over Weight")    
-calculate_bmi(weight=81,height=1.77)
+    if bmi < 18.5:
+        return -1  # Underweight
+    elif 18.5 <= bmi < 25:
+        return 0   # Normal weight
+    else:
+        return 1   # Overweight
 
+# Example usage
+result = calculate_bmi(weight=81, height=1.77)
+print("Classification Code:", result)
