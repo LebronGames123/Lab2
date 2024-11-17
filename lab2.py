a6a96859543078ce04054f2ddc361de07f6ae6c7
Jaeson <jaesonmathew.22@ichat.sp.edu.sg>
2024-11-18 04:51:31 +0800

    added functions

--- lab2.py
+++ lab2.py
@@ -1,35 +1,42 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
-
 def display_main_menu():
-    x = input("Enter some numbers separated by commas (e.g. 5,67,32): ")
-    
-    listofnumbers = [float(num.strip()) for num in x.split(",")]
-    return listofnumbers
-    
+    user_input = input("Enter some numbers separated by commas (e.g. 5, 67,32)")
+    formatted_input = user_input.split(",")
+    int_list=list(map(int,formatted_input))
+    print(f"Your list = {int_list}")
+    return(int_list)
 
-def calc_average_temperature(values):
-    return sum(values)/len(values)
+def calc_min(lists):
+    return min(lists)
 
-def calc_median_value(median):
-    sorted_median= sorted(median)
-    n = len(sorted_median)
+def calc_max(lists):
+    return max(lists)
 
-    if n%1==1:
-        median = sorted_median=[n//2]
+def calc_avg(lists):
+    return sum(lists)/len(lists)
+
+def calc_median(lists):
+    sorted_list=sorted(lists)
+    if len(sorted_list)%2 ==1:
+        median_index=sorted_list//2
+        return sorted_list[median_index]
     else:
-        median = (sorted_median[n//2-1] + sorted_median[n//2])/2
-
-    return median        
-
+        mid1 = len(sorted_list)//2
+        mid2 = len(sorted_list)//2-1
+        midvalue = (sorted_list[mid1]+sorted_list[mid2])/2
+        return midvalue
 
 def main():
-    output = display_main_menu()
-    averageval= calc_average_temperature(output)
-    medianval=calc_median_value(output)
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+    user_input=display_main_menu()
+    maxval=calc_max(user_input)
+    print(f"The max value in the list = {maxval}")
+    minval=calc_min(user_input)
+    print(f"The min value in the list = {minval}")
+    avgval=calc_avg(user_input)
+    print(f"The average value in the list = {avgval}")
+    median = calc_median(user_input)
+    print(f"The median value = {median:.2f}")
 
-    print("Average value =",averageval)
-    print("Median value =",medianval)
 
-
-main()
-
+if __name__ == "__main__":
+    main()
